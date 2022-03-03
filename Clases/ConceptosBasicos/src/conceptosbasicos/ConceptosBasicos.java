2c26b6d887720c164a67023d8ad7293b5ce14e23
ana-rosales <anapaula161103@gmail.com>
2022-03-02 19:01:51 -0600

    Sesión 1 marzo, automóvil

--- Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -4,6 +4,10 @@
  */
 package conceptosbasicos;
 
+import ico.fes.Automovil;
+import ico.fes.Silla;
+import java.awt.Color;
+
 /**
  *
  * @author Jibda
@@ -16,6 +20,26 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println( s1 ); //imprime dirección de memoria del objeto
+                                  //si se tiene toString() lo invoca automáticamente
+        System.out.println(s1.toString());
+        
+        //método constructor
+        Automovil miBocho = new Automovil();
+        System.out.println(miBocho);
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.BLUE);
+        System.out.println(miBocho);
+        
+        //método constructor sobrecargado
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustang);
+        
+        Automovil miAkura = new Automovil("Akura", "NSX", 2013, Color.GRAY);
+        System.out.println(miAkura);        
     }
     
 }
